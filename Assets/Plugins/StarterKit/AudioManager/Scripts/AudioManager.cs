f47ce0689461095e52be783acd760a0203fbb143
Kanokwan Pinthong <kw.pinthong@gmail.com>
2024-12-05 17:37:41 +0700

    update audio manager mute

--- Assets/Plugins/StarterKit/AudioManager/Scripts/AudioManager.cs
+++ Assets/Plugins/StarterKit/AudioManager/Scripts/AudioManager.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using DG.Tweening;
 using StarterKit.Common;
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace StarterKit.AudioManagerLib
 {
@@ -11,31 +12,63 @@ namespace StarterKit.AudioManagerLib
         
         public static void PlayBGM(string bgmId)
         {
-            if (instance == null)
-                CreateInstance.Create(nameof(AudioManager));
+            EnsureCreateInstance();
             instance.ThisPlayBGM(bgmId);
         }
 
         public static void PlaySound(string soundId)
         {
-            if (instance == null)
-                CreateInstance.Create(nameof(AudioManager));
+            EnsureCreateInstance();
             instance.ThisPlaySound(soundId);
         }
+        
+        public static void SetBGMVolume(float volume) => SetVolume(instance.adioMixerBGMParmName, volume);
 
+        public static void SetSoundVolume(float volume) => SetVolume(instance.adioMixerSFXParmName, volume);
+
+        public static void MuteBGM(bool isMute) => SetVolume(instance.adioMixerBGMParmName, isMute ? 0f : instance.BGMVolume);
+
+        public static void MuteSound(bool isMute) => SetVolume(instance.adioMixerSFXParmName, isMute ? 0f : instance.SFXVolume);
+        
+        private static void SetVolume(string paramName, float volumeLevel)
+        {
+            EnsureCreateInstance();
+            
+            var finalVolume = Mathf.Clamp(volumeLevel, 0.0001f, 1f);
+            var volume = Mathf.Log10(finalVolume) * 20;
+            
+            instance.audioMixer.SetFloat(paramName, volume);
+        }
+        
+        private static void EnsureCreateInstance()
+        {
+            if (instance == null)
+                CreateInstance.Create(nameof(AudioManager));
+        }
+        
         [SerializeField] 
         private AudioBank audioBank;
         [SerializeField]
         private AudioSource bgmSource;
         [SerializeField]
         private AudioSource sfxSource;
+        [SerializeField]
+        private AudioMixer audioMixer;
+        [SerializeField]
+        private string adioMixerBGMParmName = "BGM";
+        [SerializeField]
+        private string adioMixerSFXParmName = "SFX";
         
+        [Header("BGM Setting")]
         [SerializeField]
         private float bgmCrossFadeDuration = 1f;
 
         private AudioSource bgmSource1;
         private AudioSource bgmSource2;
         private List<AudioSource> sfxSources = new List<AudioSource>();
+        
+        private float BGMVolume => PlayerPrefs.GetFloat("StarterKit/AudioManager/BGMVolume", 1f);
+        private float SFXVolume => PlayerPrefs.GetFloat("StarterKit/AudioManager/SFXVolume", 1f);
 
         private void Awake()
         {
